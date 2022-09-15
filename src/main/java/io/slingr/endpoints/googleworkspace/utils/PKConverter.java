97b1a4715f52aa67d79f13c384d25838e3496c04
german <germanmanzur98@gmail.com>
2022-09-15 16:42:41 -0300

    change base 64 decoder

--- src/main/java/io/slingr/endpoints/googleworkspace/utils/PKConverter.java
+++ src/main/java/io/slingr/endpoints/googleworkspace/utils/PKConverter.java
@@ -7,14 +7,10 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Base64;
 
-/*
- <p>
- */
 public class PKConverter {
-    
     public static PrivateKey setPrivateKey(String privateKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
         String privateKeyContent = privateKey.replaceAll("\\n", "").replace("-----BEGIN PRIVATE KEY-----", "").replace("-----END PRIVATE KEY-----", "");
-        PKCS8EncodedKeySpec keySpecPKCS8 = new PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKeyContent));
+        PKCS8EncodedKeySpec keySpecPKCS8 = new PKCS8EncodedKeySpec(Base64.getMimeDecoder().decode(privateKeyContent));
         KeyFactory kf = KeyFactory.getInstance("RSA");
         return kf.generatePrivate(keySpecPKCS8);
     }
